62533519132f05289c904eba83f3f91da829c387
Korada <lkorada@deloitte.com>
2019-05-13 17:54:09 +0530

    Added API call and integrated it with the view

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from '../containers/Button';
 import NewsItem from '../containers/NewsItem';
+import Loading from '../containers/Loading';
 
 function App() {
   return (
